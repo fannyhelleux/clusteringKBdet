d494127761ad344bd78f3084715a7d2cc1cabb77
fannyhelleux <73939991+fannyhelleux@users.noreply.github.com>
2022-10-20 13:26:55 +0100

    Addition of description paragraph

--- imgWeatherSorting.py
+++ imgWeatherSorting.py
@@ -1,11 +1,13 @@
-# Different parameters :
+"""
+This program sort through different images from a folder (path as input). First it calculates their average luminosity, their
+luminosity variance as well as the number of pixel above a certain threshold (function : imgCalculation). Then it creates a
+standardized dataframe (mean = 0 and standard dev =1) that is then clusterized using a k-mean method. The images are then
+copied to new folder depending on their cluster numbers
 
-#  This parameter allows you to modify the sensitivity to light of the algorithm
-thresh_whitePX = 0.4
-# This parameter determines the number of cluster created by the algorithm
-nb_cluster = 2
+Other sorting characteristics can be added if necessary (gradient mean, ect.)
+"""
 
-# libraries import
+# Libraries import :
 import os
 import shutil
 import cv2
@@ -16,8 +18,24 @@ from sklearn.cluster import KMeans
 from sklearn.preprocessing import StandardScaler
 
 
+# Parameters :
+# Modify the sensitivity to light of the algorithm (0<x<1)
+thresh_whitePX = 0.9
+# Number of cluster created by the algorithm
+nb_cluster = 4
+
+path = input(
+    "\nEnter the path of the folder containing the images that need to be sorted: \nex : C:/Users/33608/OneDrive - Fondation EPF/Desktop/ERI/Shag image treatement/october2019/ \n ")
+# Get the path of the directory containing the images
+
+print("Processing ...")
+
+
+
 def imgCalculation(imgName):
-    # function taking a file name (RGB jpg image) as input, converting as B/W and returning the luminosity variance
+    # function taking a file name (RGB jpg image) as input, converting as B/W and returning the luminosity variance,
+    # mean and number of pixel above thresh_whitePX
+
     # input : imgName (String)
     # output : varImg_grey, meanImg_grey, whitePX
 
@@ -36,11 +54,6 @@ def imgCalculation(imgName):
 
     return varImg_grey, meanImg_grey, whitePX
 
-
-# Get the list of all files in the directory
-path = input(
-    "\nEnter the path of the folder containing the images that need to be sorted: \nex : C:/Users/33608/OneDrive - Fondation EPF/Desktop/ERI/Shag image treatement/october2019/ \n ")
-print("Processing ...")
 # Recovery of all the file's name located in the chosen directory
 dir_list = os.listdir(path)
 
@@ -56,14 +69,15 @@ whitePXList = []
 
 res = [2]
 
+# For each image calculate the different parameters and fill the specified list with those
 for i in imgName_List:
     res = imgCalculation(i)
     varList.append(res[0])
     meanList.append(res[1])
     whitePXList.append(res[2])
 
-# creates a dataframe containing the variances
-df = pd.DataFrame({'Variances ': varList, 'Means': meanList, 'Number of white PX (<0.4)': whitePXList})
+# creates a dataframe containing the variances, means and number of white pixel superior to the threshold
+df = pd.DataFrame({'Variances ': varList, 'Means': meanList, 'Number of white PX (<thresh_whitePX)': whitePXList})
 
 # create scaled DataFrame where each variable has mean of 0 and standard dev of 1
 scaled_df = StandardScaler().fit_transform(df)
